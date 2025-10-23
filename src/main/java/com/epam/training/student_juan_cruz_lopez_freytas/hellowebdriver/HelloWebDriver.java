5aa7661cf8799f2483dd4b8a17e920b428a2fd76
Juan Cruz Lopez Freytas <juancruzlopezfreytas@gmail.com>
2025-10-22 21:23:49 -0300

    Hello WebDriver continuation

--- src/main/java/com/epam/training/student_juan_cruz_lopez_freytas/hellowebdriver/HelloWebDriver.java
+++ src/main/java/com/epam/training/student_juan_cruz_lopez_freytas/hellowebdriver/HelloWebDriver.java
@@ -1,20 +1,44 @@
 package com.epam.training.student_juan_cruz_lopez_freytas.hellowebdriver;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.function.Function;
 
 public class HelloWebDriver {
+
     public static void main(String[] args) throws InterruptedException {
+
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.selenium.dev/");
-        WebElement buttonPlaceholder = driver.findElement(By.className("DocSearch-Button-Placeholder"));
-        buttonPlaceholder.click();
-        WebElement searchInput = driver.findElement(By.id("docsearch-input"));
-        searchInput.sendKeys("selenium java" + Keys.ENTER);
-        Thread.sleep(5000);
+        driver.get("https://mvnrepository.com/");
+
+        WebElement searchInput = waitUntilElementLocated(driver, By.id("query"));
+        searchInput.sendKeys("selenium java");
+
+        WebElement searchButton = driver.findElement(By.xpath("//*[@id='search']/form/input[2]"));
+        searchButton.click();
+
+        Thread.sleep(10000); // Recordá que este sleep es solo para ver el resultado, no es buena práctica.
+
         driver.quit();
+    }
+
+    private static WebElement waitUntilElementLocated(WebDriver driver, By by) {
+
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(15))
+                .pollingEvery(Duration.ofSeconds(3))
+                .ignoring(NoSuchElementException.class);
+
+        return wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(by);
+            }
+        });
     }
 }
